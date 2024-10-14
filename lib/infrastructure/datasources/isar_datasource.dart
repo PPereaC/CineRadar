57be943553d2a889394db692277f1b4971b45ad1
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2024-10-14 14:23:52 +0200

    add: implementación IsarDatabase + métodos

---
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -0,0 +1,77 @@
+import 'package:cinehub/domain/datasources/local_storage_datasource.dart';
+import 'package:cinehub/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class IsarDatasource extends LocalStorageDatasource {
+
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [MovieSchema],
+        directory: dir.path,
+        inspector: true
+      );
+    }
+
+    return Future.value(Isar.getInstance());
+
+  }
+
+  @override
+  Future<bool> isMovieFavorite(int movieId) async {
+
+    final isar = await db;
+    final Movie? isFavoriteMovie = await isar.movies
+      .filter()
+      .idEqualTo(movieId)
+      .findFirst();
+
+    return isFavoriteMovie != null;
+
+  }
+
+
+  @override
+  Future<void> toggleFavorite(Movie movie) async {
+    
+    final isar = await db;
+
+    final favoriteMovie = await isar.movies
+      .filter()
+      .idEqualTo(movie.id)
+      .findFirst();
+
+    if (favoriteMovie != null) { // Borrar la película de la base de datos
+      isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
+      return;
+    }
+
+    // Insertar la película en la base de datos
+    isar.writeTxnSync(() => isar.movies.putSync(movie));
+
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    
+    final isar = await db;
+
+    return isar.movies
+      .where()
+      .offset(offset)
+      .limit(limit)
+      .findAll();
+
+  }
+
+}
