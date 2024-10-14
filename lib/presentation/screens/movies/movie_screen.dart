e97c16f298cce3fdd62de2b5b73791edc7b7f330
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2024-10-14 13:16:54 +0200

    update: Reestructuración en widgets

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,7 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:icons_plus/icons_plus.dart';
 
 import '../../../domain/entities/movie.dart';
@@ -45,7 +44,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
       body: CustomScrollView(
         physics: const ClampingScrollPhysics(),
         slivers: [
-          _CustomSliverAppBar(movie: movie),
+          CustomSliverAppBar(movie: movie),
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (context, index) => _MovieDetails(movie: movie),
@@ -54,75 +53,6 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
           )
         ],
       ),
-    );
-  }
-}
-
-class _CustomSliverAppBar extends StatelessWidget {
-
-  final Movie movie;
-
-  const _CustomSliverAppBar({required this.movie});
-
-  @override
-  Widget build(BuildContext context) {
-
-    final size = MediaQuery.of(context).size;
-
-    return SliverAppBar(
-      backgroundColor: Colors.black,
-      expandedHeight: size.height * 0.7,
-      foregroundColor: Colors.white,
-      leading: IconButton(
-        onPressed: () {
-          context.pop(context);
-        },
-        icon: const Icon(Icons.arrow_back_ios_new_outlined)
-      ),
-      flexibleSpace: FlexibleSpaceBar(
-        background: Stack(
-          children: [
-
-            SizedBox.expand(
-              child: Image.network(
-                movie.posterPath,
-                fit: BoxFit.cover,
-                loadingBuilder: (context, child, loadingProgress) {
-                  if (loadingProgress != null) return const SizedBox();
-                  return FadeIn(child: child);
-                },
-              ),
-            ),
-
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.9, 1.0],
-                    colors: [Colors.transparent, Colors.black87],
-                  )
-                )
-              ),
-            ),
-
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    stops: [0.0, 0.3],
-                    colors: [Colors.black87, Colors.transparent],
-                  )
-                )
-              ),
-            ),
-
-          ],
-        ),
-      ),
-
     );
   }
 }
